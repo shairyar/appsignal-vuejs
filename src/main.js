86fe01f130f5cd1fe2b533e3958cbf593efa77a0
Shairyar Baig <shairyarbaig@gmail.com>
2021-10-08 16:33:38 +0500

    Integrate AppSignal

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Appsignal from "@appsignal/javascript";
+import { errorHandler } from "@appsignal/vue";
+
+const appsignal = new Appsignal({
+  key: "YOUR-FONT-END-KEY",
+});
+
 Vue.config.productionTip = false
+Vue.config.errorHandler = errorHandler(appsignal, Vue);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
